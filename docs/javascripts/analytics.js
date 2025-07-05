0c1e03a16d474a3cb24426284244b762a9c8b420
wit543 <norawit543@hotmail.com>
2025-07-05 13:53:28 +0700

    Integrate Google Tag Manager

--- docs/javascripts/analytics.js
+++
@@ -1,6 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-if (typeof gtag === 'function') {
-  gtag('js', new Date());
-  gtag('config', 'UA-XXXXXX-Y');
-}
